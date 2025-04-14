2f00adb4279885d4b0f1552a2b809f02c18e6db7
Monu Yadav <154664881+Monuyadav18@users.noreply.github.com>
2025-04-14 19:16:08 +0000

    memory

--- 01_basics/dataTypes-Summary.js
+++ 01_basics/dataTypes-Summary.js
@@ -31,3 +31,28 @@ const myFunction = function(){
     console.log("Hello Coders!")
 }
 
+// +++++++++++++++++++++++++++++++++++++++++
+
+// 2 Types of Memory
+
+// (1) -> stack (primitive) : store the copy value.
+
+let myfirstName = "Monu";
+
+let myLastName = myfirstName;
+myLastName = "Yadav";
+
+console.log(myfirstName);
+console.log(myLastName);
+
+// (2) -> Heap (Non-primitive) : store the reference/ original value.
+
+let user1 = {
+    email: "user@google.com",
+    age: 22
+}
+let user2 = user1;
+user2.email = "monu@google.com";
+
+console.log(user1.email);
+console.log(user2.email);
