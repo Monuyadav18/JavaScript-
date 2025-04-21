c1e8f0e83dba8f71269b358b6121ed323986ee60
Monu Yadav <154664881+Monuyadav18@users.noreply.github.com>
2025-04-21 18:06:18 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,7 +3,46 @@ let a = 300;
 if (true) {
     let a = 10;
     const b = 20;
-    console.log("Inner a =", a);
+    // console.log("Inner a =", a);
 }
 
-console.log("Outer a =",a)
+// console.log("Outer a =",a)
+
+function one (){
+    const username = "Monu";
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+
+// one();
+
+if(true) {
+    const username = "Monu";
+    if(username === "Monu"){
+        const website = "youtube";
+        console.log(username + website);
+}
+// console.log(website);
+}
+
+// console.log(username);
+
+//***********  interesting   ***************
+
+console.log(addone(6));
+
+function addone(num){
+    return num + 1;
+}
+
+// addTwo(5); can't access before initialization
+const addTwo = function(num){
+    return num + 2;
+}
+console.log(addTwo(5));
